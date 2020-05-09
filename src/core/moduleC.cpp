d6f4bfb9c9541cc14e90192c14ddbf4d11071c26
Bao <dqbaoptit@gmail.com>
2020-05-09 11:05:11 +0700

    fixed module C

--- src/core/moduleC.cpp
+++ src/core/moduleC.cpp
@@ -1,57 +1,7 @@
 #include "library.h"
-using namespace std;
 
-#define MAX 50
-
-lopCQ DSLopCQ[MAX];
-short counter=0;
-void POST(char* tenLop, LinkedList<Student> dssv){
-    lopCQ(tenLop, dssv);
-    counter++;
-}
-void GET_ALL(){
-    for(int i=0;i<counter;i++){
-        cout << DSLopCQ[i].get_TenLop() << '\n';
-    }
-}
-void GET_NAME(char* name){
-    bool find=false;
-    for(int i=0;i<counter && !find ;i++){
-        if(name == DSLopCQ[i].get_TenLop()){
-            cout << DSLopCQ[i].get_TenLop() << "\n";
-            find=true;
-        }
-    }
-    cout << (find==false)?"Khong tim thay lop\n" : "\n";
-}
-void PUT(char* name, lopCQ tmp){
-    bool find=false;
-    for(int i=0;i<counter && !find ;i++){
-        if(name == DSLopCQ[i].get_TenLop()){
-            DSLopCQ[i]= tmp;
-            find=true;
-        }
-    }
-    cout << (find==false)?"Khong tim thay lop\n" : "\n";
-}
-
-LinkedList<Student> SelectClass(){
-    char* name;
-    cout << "Nhap vao Lop ban muon them sinh vien : \n";
-    cin >> name;
-    bool find=false;
-    for(int i=0;i<counter && !find ;i++){
-        if(name == DSLopCQ[i].get_TenLop()){
-            return DSLopCQ[i].get_DSSV();
-        }
-    }
-    cout << "Khong tim thay lop nay\n";
-}
-
-
-void Add(){
-    LinkedList<Student> SelectedClass = SelectClass(); 
-    cout << "Nhap thong tin sinh vien\n";
+void Add(LinkedList<Student>& dssv, char* MaLopCQ){
+    std::cout << "Nhap thong tin sinh vien\n";
     char* MASV="TEST"; 
     char* HO="TEST";
     char* TEN="TEST";
@@ -60,5 +10,5 @@ void Add(){
     bool PHAI=1;
     
     Student sv(MASV, HO, TEN, PHAI, SDT, MALOP);
-    SelectedClass.push_back(sv);
+    dssv.push_back(sv);
 }
