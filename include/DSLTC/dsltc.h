2378bd1b0d2c9a4e370901246ee54d111281da38
Trần Minh Thắng <roccm0200@gmail.com>
2020-05-13 12:54:10 +0700

    Update dsltc.h

--- include/DSLTC/dsltc.h
+++ include/DSLTC/dsltc.h
@@ -22,40 +22,40 @@ struct Lop {
 	int sv_max;
 	int sv_min;
 	DanhSachSinhVienDK * dsdk; 
-  
-  Lop(char *, short, short, short, int, int);
-  Lop() {;}
 
-  void save();
+	Lop(char *, short, short, short, int, int);
+	Lop() {;}
+
+  	void save();
 };
 
 class LOPTC {
-	private:
-		int n = 0;
-		int stt = 0;
-    char db[32] = "../../database/dsltc.d";
+private:
+	int n = 0;
+	int stt = 0;
+	char db[32] = "../../database/dsltc.d";
 	
-  public:
-		Lop *node[LOP_MAX];
+public:
+	Lop *node[LOP_MAX];
 		
-    Lop* getLop();
-		int get_STT();
-    void setLop(Lop* &lop_tmp);
-		int getN();
-		void setN(int n_tmp);
-		void khoiTaoDS();
-		bool isFull();
-		bool isEmpty();
-		void insertLast(Lop &lop);
-		void insertOrder(Lop &lop, int pos);
-		int search(int malop_tmp);
-		void xuatDS();
-		void nhapLop(Lop &lop);
-		void themLop(Lop &lop, int pos);
-		void xoaLop(int malop_del);
-		void xoaDS();
-		void thongBao(char* s);
-    void save();
-    void load();
+    	Lop* getLop();
+	int get_STT();
+    	void setLop(Lop* &lop_tmp);
+	int getN();
+	void setN(int n_tmp);
+	void khoiTaoDS();
+	bool isFull();
+	bool isEmpty();
+	void insertLast(Lop &lop);
+	void insertOrder(Lop &lop, int pos);
+	int search(int malop_tmp);
+	void xuatDS();
+	void nhapLop(Lop &lop);
+	void themLop(Lop &lop, int pos);
+	void xoaLop(int malop_del);
+	void xoaDS();
+	void thongBao(char* s);
+	void save();
+	void load();
 };
 #endif
