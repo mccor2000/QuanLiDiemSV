c6e8348e1718dff93fabd13b529987f690e4f7de
mccor2000 <roccm0200@gmail.com>
2020-05-14 17:18:34 +0700

    Update moduleE.cpp

--- include/DSMH/dsmh.h
+++ include/DSMH/dsmh.h
@@ -1,9 +1,9 @@
-#ifndef _DSMH_H
-#define _DSMH_H
+#pragma once
+
 #include "../templates/LinkedList.h"
 #include <fstream>
 #include <cstring>
-#include <iostream>
+
 /****************** MON HOC *******************/ 
 class MonHoc {
 public:
@@ -51,12 +51,14 @@ public:
   void insert(MonHoc);
   void remove(MonHoc);
   
+  bool is_exist(MonHoc);
+
   MonHoc * search_name(char *);
   MonHoc * search_code(char *);
   
-  void enumerate();
-  LinkedList<char*> list_mh(node *);
+  LinkedList<MonHoc> enumerate(); // Return list of MonHoc
   
+  //** File ops
   void load();
   void save();
 
@@ -69,10 +71,12 @@ private:
   
   node * insert_node(node *, MonHoc);
   node * remove_node(node *, MonHoc);
+  
   MonHoc * search_name_node(node *, char *);
   MonHoc * search_code_node(node *, char *);
+  
+  bool check_exist(node *, MonHoc);
 
-  void in_order(node *);
+  void in_order(node *, LinkedList<MonHoc>&);
   void save_node(node *, std::ofstream &);
 };
-#endif
