7f6d8f1646400e11a7c6d83cd5c16cfce7adb122
mccor2000 <roccm0200@gmail.com>
2020-05-17 12:45:33 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,46 @@
 
 
 int main() {
+  /* Initialize ncurses */
   initscr();
   cbreak();
   noecho();
   keypad(stdscr, TRUE);
-init_pair(1, COLOR_RED, COLOR_BLACK);
-  WINDOW * wins[2];
-  PANEL * panels[2];
+  init_pair(1, COLOR_RED, COLOR_BLACK);
   int rows, cols;
-
-  /* Get scr size */
   getmaxyx(stdscr, rows, cols);
   
-  /* Create windows */
-  wins[0] = newwin(rows, (int)cols/4, 0, 0);
-  wins[1] = newwin(rows, (int)(3*cols)/4, 0, (int)cols/4 + 1);
-  
-  keypad(wins[0], TRUE);
-  keypad(wins[1], TRUE);
-  
-  box(wins[0], 0, 0);
-  box(wins[1], 0, 0);
-  refresh();
 
+  /* Initialize 2 panels contain windows */
+  WINDOW * wins[2];
+  PANEL * panels[2];
+  
+  //-- Menu window
+  wins[0] = newwin(rows, (int)cols/4, 0, 0);
+  keypad(wins[0], TRUE);
+  box(wins[0], 0, 0);
+  panels[0] = new_panel(wins[0]);
+  //-- Display window
+  wins[1] = newwin(rows, (int)(3*cols)/4, 0, (int)cols/4 + 1);
+  keypad(wins[1], TRUE);
+  box(wins[1], 0, 0);
+  panels[1] = new_panel(wins[1]);
+  
+  update_panels();
+  doupdate();
+  refresh();
+  
+  /* Inital status */
   MENU * core_menu = main_menu();
   set_menu_win(core_menu, wins[0]);
   set_menu_sub(core_menu, derwin(wins[0], 0, 0, 1, 1));
-
   post_menu(core_menu);
   wrefresh(wins[0]);
-
-  print_in_middle(stdscr, rows/2, (int)cols/4 + 2, 40, "QUAN LI DIEM SINH VIEN", COLOR_PAIR(1));
-
+  
+  print_in_middle(wins[1], 8, 0, 80, "QUAN LI DIEM SINH VIEN", COLOR_PAIR(1));
+  wrefresh(wins[1]);
+  
+  /* Core app's functions */
   int c;
   while((c = wgetch(wins[0])) != KEY_F(1)) {
     switch(c) {
@@ -43,8 +51,11 @@ init_pair(1, COLOR_RED, COLOR_BLACK);
       case KEY_UP:
         menu_driver(core_menu, REQ_UP_ITEM);
         break;
+      case 10:
+        break;
     }
     wrefresh(wins[0]);
   }
+
   endwin();
 }
