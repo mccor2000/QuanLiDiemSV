9d67b30d5a5d5a286a7cb5de03db53296d88d56f
HAU <conghau7620.ptit@gmail.com>
2020-05-18 12:39:13 +0700

    update moduleJ

--- src/core/moduleJ.cpp
+++ src/core/moduleJ.cpp
@@ -1,30 +1,26 @@
 #include "library.h"
 
-void Show_Score(DanhSachMonHoc& ds, DanhSachSinhVien& dssv, LOPTC& dsltc,short nienkhoa, short hocki, short nhom, char tenMH[]){
+void Show_Score(DanhSachMonHoc& dsmh, DanhSachLopCQ &dslcq, LOPTC& dsltc,char* nienkhoa, short hocki, short nhom, char tenMH[]){
     MonHoc* mh = new MonHoc();
-    mh = ds.search_name(tenMH);
+    mh = dsmh.search_name(tenMH);
     Lop* LTC;
     bool flag=0;
     for(int i=0;i<dsltc.getN();i++){
-        if(dsltc.node[i]->maMH == mh->MAMH){
+        if(strcmp(dsltc.node[i]->maMH,mh->MAMH)==0){
             LTC = dsltc.node[i];
             flag=1;
             break;
         }
     }
     if(flag){
-    DanhSachSinhVienDK * DSDK = LTC->dsdk;
-    Node<SinhVienDK>* node_DK = DSDK->head();
-    Node<SinhVien>* node_SV = dssv.head();
-    while(node_SV!= NULL ){
-        while(node_DK!= NULL){
-            if(node_DK->get_data().get_MASV() == node_SV->get_data().get_MASV()){
-                std::cout << node_SV->get_data().get_MASV() << " " << node_SV->get_data().get_MALOP() << " "<<node_SV->get_data().get_HO() << " " << node_SV->get_data().get_TEN() << " " << node_DK->get_data().get_DIEM() << "\n";
-            }
+        DanhSachSinhVienDK * DSDK = LTC->dsdk;
+        Node<SinhVienDK>* node_DK = DSDK->head();
+        while (node_DK!=NULL) {
+            SinhVien sv = timSV(node_DK->get_data().get_MASV(),dslcq)->get_data();
+            cout<<sv.get_MALOP()<<"\t"<<sv.get_MASV()<<"\t"<<sv.get_HO()<<"\t"<<sv.get_TEN()<<"\t"<<
+                node_DK->get_data().get_DIEM()<<"\n";
             node_DK = node_DK->get_next();
         }
-        node_SV = node_SV->get_next();
-    }
     }
     else{
         std::cout << "Mon hoc khong ton tai\n";
