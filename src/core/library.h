d3626d9b8588ed6b63c98aad79586a6955b7888a
HAU <conghau7620.ptit@gmail.com>
2020-05-18 10:36:07 +0700

    update library.h

--- src/core/library.h
+++ src/core/library.h
@@ -16,35 +16,35 @@
 // Modules
 
 //-- A
-void themLop(LOPTC &loptc);
+void themLop(LOPTC &loptc, Lop &lop);
 void xoaLop(LOPTC &loptc,int maloptc);
 void hieuChinhLop(LOPTC &loptc, int index);
 
 //-- B
-void in_danh_sach_dk(LOPTC, short, short, short, char *);
+void in_danh_sach_dang_ki(LOPTC dsltc, char* nienkhoa, short hocki, short nhom, char * maMH);
 
 //-- C
-void them_sv(DanhSachLopCQ&, char *,SinhVien&);
-int vitri_sv(DanhSachSinhVien*,char*);
-void xoa_sv(DanhSachLopCQ&, char *, char*);
-void hieu_chinh_sv(DanhSachLopCQ &, char *, char*);
+void them_sv(DanhSachLopCQ &DSLCQ, char * ma_lop, SinhVien &sv);
+int vitri_sv(DanhSachSinhVien* DSSV,char* MASV);
+void xoa_sv(DanhSachLopCQ &DSLCQ, char * ma_lop, char* MASV);
+void hieu_chinh_sv(DanhSachLopCQ &DSLCQ, char * ma_lop, char* MASV);
 
 //-- D
-void in_danh_sach_sv(DanhSachLopCQ, char *);
+void in_danh_sach_sv(DanhSachLopCQ DSLCQ, char * ma_lop);
 
 //-- E
-void them_mh(DanhSachMonHoc&);
-void hieu_chinh_mh(DanhSachMonHoc&);
-void xoa_mh(DanhSachMonHoc&);
+void them_mh(DanhSachMonHoc& dsmh, MonHoc new_mh);
+void hieu_chinh_mh(DanhSachMonHoc& dsmh, MonHoc old_mh, MonHoc new_mh);
+void xoa_mh(DanhSachMonHoc& dsmh, MonHoc mh);
 
 //-- F
-void print_dsmh(DanhSachMonHoc&);
+void in_dsmh(DanhSachMonHoc& dsmh);
 
 //-- G
-Node<SinhVien>* timSV(char *, DanhSachLopCQ &dslcq);
-void xuatSV(SinhVien * SV);
-void locDSLTC(LOPTC &loptc, DanhSachMonHoc &dsmh , short nienkhoa_tmp, short hocki_tmp);
-int dangKyLop(DanhSachSinhVienDK& , SinhVienDK);
+Node<SinhVien>* timSV(char* maSV_tmp, DanhSachLopCQ &dslcq);
+void xuatSV(Node<SinhVien>* SV);
+void locDSLTC(LOPTC &loptc, DanhSachMonHoc &dsmh , char* nienkhoa_tmp, short hocki_tmp);
+int dangKyLop(Lop &loptc, SinhVienDK SV);
 
 //-- H
 void DSLopTC_HUY(LOPTC& BASE_DSLTC);
@@ -53,11 +53,12 @@ bool HuyLopTC(LOPTC& BASE_DSLTC,int ma_lop);
 
 
 //-- I
-Lop* timLop(LOPTC &dsltc, short nienkhoa_tmp, short hocki_tmp, short nhom_tmp, char* maMH_tmp);
-void xuatDSDK(Lop* lop, LinkedList<SinhVien> &dssv);
+Lop* timLop(LOPTC &dsltc, char* nienkhoa_tmp, short hocki_tmp, short nhom_tmp, char* maMH_tmp);
+void xuatDSDK(Lop* lop, DanhSachLopCQ &dslcq);
 
 //-- J
 void Show_Score(DanhSachMonHoc& ds, DanhSachSinhVien& dssv, LOPTC& dsltc,short nienkhoa, short hocki, short nhom, char tenMH[]);
+
 //-- K 
 void in_bang_diem_trung_binh(DanhSachLopCQ& dslcq, DanhSachMonHoc& dsmh, LOPTC& dsltc, char* MALOP);
 
