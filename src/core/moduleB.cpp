f093c73b501e1517215104016ded44edf4ab3d85
HAU <conghau7620.ptit@gmail.com>
2020-05-27 09:22:00 +0700

    sua ten cho dep

--- src/core/moduleB.cpp
+++ src/core/moduleB.cpp
@@ -1,11 +1,11 @@
 #include "library.h"
 
-void in_danh_sach_dang_ki(LOPTC dsltc, char* nienkhoa, short hocki, short nhom, char * maMH) {
+void in_danh_sach_dang_ki(DanhSachLopTC &dsltc, char* nienkhoa, short hocki, short nhom, char * maMH) {
   
   DanhSachSinhVienDK dsdk; 
   
   for (int i = 0; i < dsltc.getN(); i++) {
-    Lop temp = *dsltc.node[i];
+    LopTC temp = *dsltc.node[i];
     
     if (strcmp(temp.nienkhoa,nienkhoa)==0 && temp.hocki == hocki && temp.nhom == nhom && strcmp(temp.maMH, maMH) == 0) {
       dsdk = *temp.dsdk;   
