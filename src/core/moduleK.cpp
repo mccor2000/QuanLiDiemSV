871cbe05e355068d2312e2504543f07c5a11d083
Bao <dqbaoptit@gmail.com>
2020-05-31 09:22:58 +0700

    fixed something were wrong

--- src/core/moduleK.cpp
+++ src/core/moduleK.cpp
@@ -5,8 +5,7 @@ LinkedList<char* > in_bang_diem_trung_binh(DanhSachLopCQ& dslcq, DanhSachMonHoc&
   LinkedList<char* > result;
   DanhSachSinhVien * dssv = dslcq.get_dssv(MALOP);
   if(dssv == NULL){
-    std::cout << "Lop khong ton tai\n";
-    return NULL;
+    return result;
   }
   
   // Get list MonHoc
