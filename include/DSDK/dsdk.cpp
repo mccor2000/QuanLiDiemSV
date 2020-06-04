ea75c4268279ed0b5bf51ed42b53e8723afe3cc3
Bao <dqbaoptit@gmail.com>
2020-06-04 09:31:06 +0700

    add get_by_index()

--- include/DSDK/dsdk.cpp
+++ include/DSDK/dsdk.cpp
@@ -83,4 +83,17 @@ void DanhSachSinhVienDK::load(const char * ma_lop) {
   // Save file
   f.close();
 }
+SinhVienDK DanhSachSinhVienDK::get_by_index(int index){
+  Node<SinhVienDK>* node = head();
+  SinhVienDK result;
+  if(index>count()){
+    return result;
+  }
+  index--;
+  while(node!=NULL && index--){
+    node=node->get_next();
+  }
+  result=node->get_data();
+  return result;
+}
 
